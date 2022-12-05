39799da6334670e1f384ffba8142ce734be3730c
Komogi <billy.bao11@gmail.com>
2022-12-05 15:39:21 -0500

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/Floyd08/EECS4413_REPO"""
    This reverts commit 67d76b6217c4606ba36fb62b8d0af502d3fdd124.

--- EECS4413Project/src/main/java/bean/POItem.java
+++ EECS4413Project/src/main/java/bean/POItem.java
@@ -4,43 +4,32 @@ import java.util.Objects;
 
 public class POItem implements Comparable<POItem> {
 	
-	//private String ID;		//id of the item in the cart (Probably not needed)
-	private String itemID;		//id of the item in the database
-	private String itemName;	
+	private String ID;
+	private String itemID;
 	private double price;
-	private int quantity;
 	
 	//Default Constructor
 	public POItem() {
 		
-		//ID = "-1";
+		ID = "-1";
 		this.itemID = "-1";
 		this.price = 0.0;
 	}
 	
-	public POItem(String itemID, String itemName, double price) {
+	public POItem(String iD, String itemID, double price) {
 		
-		//ID = iD;
+		ID = iD;
 		this.itemID = itemID;
-		this.itemName = itemName;
 		this.price = price;
 	}
-	
-	public POItem(ItemP i) {
-		
-		this.itemID = i.getID();
-		this.itemName = i.getName();
-		this.price = i.getPrice();
-		this.quantity = 1;
+
+	public String getID() {
+		return ID;
 	}
 
-//	public String getID() {
-//		return ID;
-//	}
-//
-//	public void setID(String iD) {
-//		ID = iD;
-//	}
+	public void setID(String iD) {
+		ID = iD;
+	}
 
 	public String getItemID() {
 		return itemID;
@@ -48,14 +37,6 @@ public class POItem implements Comparable<POItem> {
 
 	public void setItemID(String itemID) {
 		this.itemID = itemID;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
 	}
 
 	public double getPrice() {
@@ -66,39 +47,36 @@ public class POItem implements Comparable<POItem> {
 		this.price = price;
 	}
 	
-	public int getQuantity() {
-		return quantity;
-	}
-	
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
 	@Override
 	public String toString() {
 		
-		return String.format("(itemID:%s itemName:%s price:%s, quantity:%s)", itemID, itemName, price, quantity);
+		return String.format("(ID:%s itemID:%s price:%s)", ID, itemID, price);
 	}
 	
 	@Override
 	public int compareTo(POItem p) {
 		
-		return this.itemID.compareTo(p.itemID);
+		return this.ID.compareTo(p.ID);
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(itemID, itemName, price, quantity);
+		return Objects.hash(ID, itemID, price);
 	}
 
 	@Override
 	public boolean equals(Object obj) {
+		
 		if (this == obj)
 			return true;
+		
 		if (!(obj instanceof POItem))
 			return false;
+		
 		POItem other = (POItem) obj;
-		return Objects.equals(itemID, other.itemID);
+		
+		return Objects.equals(ID, other.ID) && Objects.equals(itemID, other.itemID)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
 	}
 	
 	
