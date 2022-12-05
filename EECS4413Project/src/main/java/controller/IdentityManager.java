d2e5c59a1323b09d3630c483f63f524daa2c0b30
Keshav Apparasu <45578596+kkav5@users.noreply.github.com>
2022-12-05 15:28:03 -0500

    Updated IdentityManager
    Passwords are hashed and responses are  given instead of throwing IllegalArgumentException

--- EECS4413Project/src/main/java/controller/IdentityManager.java
+++ EECS4413Project/src/main/java/controller/IdentityManager.java
@@ -1,57 +1,79 @@
 package controller;
 
+import java.security.NoSuchAlgorithmException;
+
 import bean.User;
 import model.Model;
 
-public static class IdentityManager{
+/**
+ * 
+ * @author Keshav Apparasu
+ *
+ */
+public final class IdentityManager{
 	
 	//Is called by CentralController for User LogIn, LogOut, registration and so forth
 	private Model dataModel = Model.getInstance();
-	private String user_id;
-	private String pwd;
-	private String name;
-	private String surName;
-	private String postalCode;
-	private String address;
 	
-	public void userExists(String id) throws IllegalArgumentException{
+	
+	public boolean userExists(String id){
 		User user = dataModel.uDB.retrieve(id);
+		boolean exist = true;
 		if (user == null) {
-			throw new IllegalArgumentException("User with this information does not exist");
+			exist = false;
 		}
+		return exist;
 	}
-	public void logIn(String id, String pass) throws IllegalArgumentException {
-		userExists(id);
-		user_id = id;
-		pwd = pass;
+	
+	public String logIn(String id, String pass) throws NoSuchAlgorithmException{
+		String res = "{\r\n"
+				+ "  \"status\": 200,\r\n"
+				+ "  \"message\": \"Log in successful\",\r\n";
+		if(!userExists(id)) {
+			res = "{\r\n"
+					+ "  \"status\": 200,\r\n"
+					+ "  \"message\": \"User does not exist\",\r\n";
+		}
 		
-		User user = dataModel.uDB.retrieve(user_id);
-		if (!((user.getID().equals(user_id) && (user.getPassword().equals(pwd))))) {
-			throw new IllegalArgumentException("Password is not correct");
+		User user = dataModel.uDB.retrieve(id);
+		if (!((user.getID().equals(id) && (user.getPassword().equals(user.hashPassword(pass)))))) {
+			res = "{\r\n"
+					+ "  \"status\": 400,\r\n"
+					+ "  \"message\": \"The password is wrong\",\r\n";
 		}
 		dataModel.makeActive(id);
+		return res;
 	}
 	
-	public void logOut(String id) {
-		userExists(id);
+	public String logOut(String id) {
+		String res = "{\r\n"
+				+ "  \"status\": 200,\r\n"
+				+ "  \"message\": \"Log in successful\",\r\n";
+		if(!userExists(id)) {
+			res = "{\r\n"
+					+ "  \"status\": 200,\r\n"
+					+ "  \"message\": \"User does not exist\",\r\n";
+		}
 		dataModel.makeInActive(id);
+		return res;
 	}
 	
-	public void register(String id, String nomi, String aile, String postal, String address, String pass) throws IllegalArgumentException {
-		boolean flag = false;
-		try {
-			userExists(id);
-			flag = true;
-		}catch(IllegalArgumentException e) {
-			//User object creation
-			User user = new User(user_id, name, surName, postalCode, address, pwd);
+	public String register(String id, String nomi, String aile, String postal, String address, String pass) throws NoSuchAlgorithmException{
+		String res = "{\r\n"
+				+ "  \"status\": 200,\r\n"
+				+ "  \"message\": \"Log in successful\",\r\n";
+		if(!userExists(id)) {
+			res = "{\r\n"
+					+ "  \"status\": 200,\r\n"
+					+ "  \"message\": \"User does not exist\",\r\n";
+		}else {
+			User user = new User(id, nomi, aile, postal, address, pass);
+			user.changePassword(user.hashPassword(pass), pass);
 			dataModel.uDB.insert(user);
-			logIn(user_id, pwd);
+			logIn(id, pass);
 		}
-
-		if(flag) {
-			throw new IllegalArgumentException("User with this ID already exists");
-		}
+		
+		return res;
 	}
 	
 	
